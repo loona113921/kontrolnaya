851aa0a09b996a052d858db76fee6e9f4fa77488
Yuliya Ustimenko <loona113921@gmail.com>
2024-05-18 23:25:00 +0300

    Сохранила решение с ошибкой, буду дальше решать

--- Zadanie/Program.cs
+++ Zadanie/Program.cs
@@ -1,1 +1,19 @@
-﻿
+﻿using System.Runtime.CompilerServices;
+
+void Main()
+{
+
+string [] massiv = {'Hello','2','world',':-)'};
+string str = "";
+
+
+foreach (string sim in massiv)
+
+if (str = massiv.Length <= 3)
+{
+System.Console.WriteLine(str);
+}
+
+
+}
+Main();
